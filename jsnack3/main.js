1db75fb46c2735b4f1cb67c28315a3f6433d4951
Gioia Ascari <gioia.ascari96@gmail.com>
2022-03-11 10:43:33 +0100

    esercizio corretto

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,10 +1,16 @@
 console.log("Ok Js");
 //Chiedi un numero di 4 cifre all’utente e
 //calcola la somma di tutte le cifre che compongono il numero.
+let numero;
+while (isNaN(parseInt(numero)) || numero.length !== 4) {
+  numero = prompt("inserisci un numero di 4 cifre");
+}
 
-let numero = prompt("inserisci un numero di 4 cifre");
 let somma = 0;
-for (i = 0; i < numero.length; i++) {
-  somma = somma + parseInt(numero[i]);
+
+for (let i = 0; i < numero.length; i++) {
+  const carattere = numero;
+  somma += parseInt(carattere);
 }
 console.log(somma);
+//Esercizio corretto, mancava lo step del while
